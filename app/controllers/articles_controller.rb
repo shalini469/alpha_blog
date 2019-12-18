468b12d8627ac27498b99691008807d395538bac
shalini.dixit@anchanto.com <shalini1993.sd@gmail.com>
2019-12-18 15:32:42 +0530

    show method updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,17 @@ class ArticlesController < ApplicationController
     end
 
     def create
-       # render plain: params[:article].inspect
-        @article=Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@article)
-    end
+       @article=Article.new(article_params)
+       if @article.save
+        flash[:notice] = "Article was sucessfully created"
+        redirect_to article_path(@article)
+       else
+        render 'new'
+       end
+     end
+     def show
+        @article = Article.find(params[:id])
+     end
 
     private
     def article_params
