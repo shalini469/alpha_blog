3198ee3a83ec999456d81eac5724ac08c5e74d1c
shalini.dixit@anchanto.com <shalini1993.sd@gmail.com>
2019-12-19 12:25:55 +0530

    commited for new repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'articles#index'
   resources :articles
   get 'pages/home', to: 'pages#home'
   get 'pages/about', to: 'pages#about'
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
