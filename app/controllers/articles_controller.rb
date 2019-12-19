9500cc4ea91b1ccbd60c240a12ee43bbdd1c654e
shalini.dixit@anchanto.com <shalini1993.sd@gmail.com>
2019-12-19 16:17:13 +0530

    edit update  listing and other operations with validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,15 @@
 class ArticlesController < ApplicationController
-    def new
-    @article=Article.new
+    def index
+        @articles = Article.all
     end
+
+    def new
+        @article=Article.new
+    end
+
     def edit
         @article=Article.find(params[:id])
-        end
+    end
 
     def create
        @article=Article.new(article_params)
@@ -14,8 +19,9 @@ class ArticlesController < ApplicationController
        else
         render 'new'
        end
-     end
-     def update
+    end
+
+    def update
         @article=Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was sucessfully updated"
@@ -23,10 +29,11 @@ class ArticlesController < ApplicationController
            else
             render 'edit'
            end
-     end
+    end
+
      def show
-        @article = Article.find(params[:id])
-     end
+        @article=Article.find(params[:id])
+    end
 
     private
     def article_params
